28c661b7bcfa59a6f4760d6c6a5fee26ab4aa2bf
ksana-sidoruk <ksana.sidoruk@gmail.com>
2023-05-17 23:11:55 +0300

    added IfElse Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert username, please ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "anya")
+{
+    Console.WriteLine("Hey, dear Anya!");
+}
+else
+{
+    Console.Write("Oh, that's just you ");
+    Console.WriteLine(username);
+}
